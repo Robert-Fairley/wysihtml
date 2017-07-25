0596fc05b0a5bb21a693a1d4ba7c019851bc16cc
Robert-Fairley <robert.fairley@rci.rogers.com>
2017-07-25 14:03:47 -0400

    Add some custom style commands & yarn lockfile

--- dist/wysihtml.all-commands.js
+++ dist/wysihtml.all-commands.js
@@ -611,6 +611,35 @@ wysihtml.commands.superscript = (function() {
 
 })();
 
+wysihtml.commands.insertTDPinlineBlue = (function() {
+  var REG_EXP = /inline-blue-sans-h]+/g;
+
+  return {
+    exec: function(composer, command, size) {
+      wysihtml.commands.formatInline.exec(composer, command, {className: "inline-blue-sans-h", classRegExp: REG_EXP, toggle: true});
+    },
+
+    state: function(composer, command, size) {
+      return wysihtml.commands.formatInline.state(composer, command, {className: "inline-blue-sans-h"});
+    }
+  };
+})();
+
+wysihtml.commands.insertTDPinlineBlack = (function() {
+  var REG_EXP = /inline-black-sans-h]+/g;
+
+  return {
+    exec: function(composer, command, size) {
+      wysihtml.commands.formatInline.exec(composer, command, {className: "inline-black-sans-h", classRegExp: REG_EXP, toggle: true});
+    },
+
+    state: function(composer, command, size) {
+      return wysihtml.commands.formatInline.state(composer, command, {className: "inline-black-sans-h"});
+    }
+  };
+})();
+
+
 wysihtml.commands.underline = (function() {
   var nodeOptions = {
     nodeName: "U",
---
+++ src/extra-commands/todaysParentStyles.js
@@ -0,0 +1,28 @@
+wysihtml.commands.insertTDPinlineBlue = (function() {
+  var REG_EXP = /inline-blue-sans-h]+/g;
+
+  return {
+    exec: function(composer, command, size) {
+      wysihtml.commands.formatInline.exec(composer, command, {className: "inline-blue-sans-h", classRegExp: REG_EXP, toggle: true});
+    },
+
+    state: function(composer, command, size) {
+      return wysihtml.commands.formatInline.state(composer, command, {className: "inline-blue-sans-h"});
+    }
+  };
+})();
+
+wysihtml.commands.insertTDPinlineBlack = (function() {
+  var REG_EXP = /inline-black-sans-h]+/g;
+
+  return {
+    exec: function(composer, command, size) {
+      wysihtml.commands.formatInline.exec(composer, command, {className: "inline-black-sans-h", classRegExp: REG_EXP, toggle: true});
+    },
+
+    state: function(composer, command, size) {
+      return wysihtml.commands.formatInline.state(composer, command, {className: "inline-black-sans-h"});
+    }
+  };
+})();
+
